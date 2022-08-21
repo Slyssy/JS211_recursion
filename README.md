67ed798622cb68f964d4ab080abd0a967048d019
Stephen <stephen.lyssy@gmail.com>
2022-08-20 21:59:30 -0500

    Solved the other two problems.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # JS211_recursion
 
-Write a recursive JavaScript program to find the greatest common divisor (gcd)
-of two positive numbers.
+> Write a recursive JavaScript program to find the greatest common divisor (gcd)
+> of two positive numbers.
 
 <em>Example: findGCD(25, 20) // => 5 findGCD(50, 20) // => 10</em>
 
-> GCD(a,b) = (a x b) / LCM(a,b)
+> Write a recursive JavaScript program to compute the exponent of any number
+> given.
+
+<em>Example: If you were given the number 64: 8 \*\* 2 = 8 x 8 = 64. Here 8 is the base and 2 is the exponent.</em>
+
+>
+
+> Write a recursive JavaScript program to compute the sum of an array of integers.
+
+<em>Example : const array1 = [1, 2, 3, 4, 5, 6] sumArr(array1) // => Expected
+Output : 21</em>
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // $ Write a recursive JavaScript program to find the greatest common divisor (gcd) of two positive numbers.
 
-let x = 0;
 const findGCD = (a, b) => {
   if (!b) {
     return a;
@@ -9,3 +8,26 @@ const findGCD = (a, b) => {
 };
 
 console.log('GCD: ', findGCD(50, 20));
+
+// $ Write a recursive JavaScript program to compute the exponent of any number given.
+const findExponent = (num, exp) => {
+  if (exp === 0) {
+    return 1;
+  } else {
+    return num * findExponent(num, exp - 1);
+  }
+};
+console.log(findExponent(5, 3));
+
+// $ Write a recursive JavaScript program to compute the sum of an array of
+// $ integers.
+
+const sumArray = (array) => {
+  if (array.length === 1) {
+    return array[0];
+  } else {
+    return array.pop() + sumArray(array);
+  }
+};
+
+console.log('Sum of Array', sumArray([1, 2, 3, 4, 5, 6]));
